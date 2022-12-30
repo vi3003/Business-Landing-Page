3f52150c1c311f59c79ad056aa98ec6c1d6906fb
“vi3003” <vidhijain3003@gmail.com>
2022-12-30 18:16:30 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function showm(){
+    var i1 = document.getElementById('i1');
+    var i2 = document.getElementById('i2');
+    var i3 = document.getElementById('i3');
+
+    if (i1.value === '' | i2.value==='' | i3.value===''){
+        
+    }else {
+        document.getElementById('message').style.opacity = '1';
+    }
+}
